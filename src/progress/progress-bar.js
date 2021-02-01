91b08e0f0681c6b9e750f19da3fa4714ae34e2a8
Alan Massaro <alan.massar@gmail.com>
2021-02-01 16:56:19 +0100

    progress done, buttons done, style done

--- src/progress/progress-bar.js
+++ src/progress/progress-bar.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState, useRef} from "react";
 import "./progressBar.css";
+import tail from "../assets/tail.svg";
 
 const ProgressBar = props => {
     const [offset, setOffset] = useState(0);
@@ -10,21 +11,22 @@ const ProgressBar = props => {
         strokeWidth,
         circleOneStroke,
         circleTwoStroke,
+        total,
     } = props;
-
     const center = size / 2;
     const radius = size / 2 - strokeWidth / 2;
     const circumference = 2 * Math.PI * radius;
 
     useEffect(() => {
-        const progressOffset = ((100 - progress) / 100) * circumference;
+        const progressOffset = (progress / total) * circumference;
         setOffset(progressOffset);
         circleRef.current.style =
             "transition: stroke-dashOffset 850ms ease-in-out";
     }, [setOffset, circumference, progress, offset]);
 
     return (
-        <>
+        <div className={"progressbar"}>
+            <img className={"progressbar__tail"} src={tail} alt={"tail"} />
             <svg className={"svg"} width={size} height={size}>
                 <circle
                     className={"svg-circle-bg"}
@@ -45,12 +47,8 @@ const ProgressBar = props => {
                     strokeDasharray={circumference}
                     strokeDashoffset={offset}
                 />
-                <text className={"svg-circle-text"} x={center} y={center}>
-                    {progress}
-                    {":00"}
-                </text>
             </svg>
-        </>
+        </div>
     );
 };
 
